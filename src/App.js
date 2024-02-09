4e931917b5bfa02bcd1d138fa55c951ec21c3da4
dally399 <darrenkibande@gmail.com>
2024-02-09 15:08:05 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   <BrowserRouter>
+      <Navbar/>   
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route exact path='/home' element={<Home/>}></Route>
+        <Route exact path='/about' element={<AboutUs/>}></Route>
+        <Route exact path='/services' element={<Services/>}></Route>
+        <Route exact path='/gallery' element={<Gallery/>}></Route>
+        <Route exact path='/products' element={<Products/>}></Route>
+        <Route exact path='/blog' element={<Blog/>}></Route>
+        <Route exact path='/contact' element={<Contact/>}></Route>
+        <Route exact path='/faq' element={<FAQ/>}></Route>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+
     </div>
   );
 }
 
 export default App;
+
