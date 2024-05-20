0592584cc3080e9efb45d9de57a75e9fd2d9351f
darrenkibande <darrenkibande@gmail.com>
2024-05-20 13:29:37 +0300

    upd work

---
+++ frontend/src/components/Destination_info.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Destination_info.css'
+
+function Destination_info() {
+  return (
+    <div>Destination_info</div>
+  )
+}
+
+export default Destination_info
--- frontend/src/components/Tours.jsx
+++ frontend/src/components/Tours.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Tours.css'
 import pic from '../assets/elizeu-dias-SEq9dyZSe6c-unsplash.jpg'
+import { IoLocationOutline } from "react-icons/io5";
+import { FaRegCalendarAlt } from "react-icons/fa";
 
 function Tours() {
   return (
@@ -14,14 +16,13 @@ function Tours() {
   <div class="card">
     <img src={pic} alt="" class="card__image"/>
     <div class="card__info">
-      <h3 class="card__title">New York</h3>
-      <p class="card__text">$950</p>
+      <h3 class="card__title"><IoLocationOutline className='icon__tour'/>New York</h3>
+      <p class="card__text">$950<span>/Per Person</span></p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, nostrum.</p>
       <div class="card__details">
         <p class="card__detail">100 Superb</p>
-        <p class="card__detail">12 Days<span class="card__detail--highlight">71</span> Anmerica</p>
+        <p class="card__detail"><FaRegCalendarAlt className='icon__tour'/>12 Days/ 13 nights</p>
       </div>
-      <button class="card__button">Book Now</button>
     </div>
   </div>
   </div>
